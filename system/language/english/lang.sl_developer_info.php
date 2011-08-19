c93a4ed7f9987412ac88d26b5ab8c29ab84244b4
Marc Miller <marc@bigoceanstudios.com>
2011-08-19 12:47:42 -0500

    Many enhancements to the addon

--- system/language/english/lang.sl_developer_info.php
+++ system/language/english/lang.sl_developer_info.php
@@ -1,123 +1,136 @@
 <?php
 
 $L = array(
-	
-'sl_developer_info_module_name'			=> 'SL Developer Info',
-'sl_developer_info_module_description'	=> 'Collates Weblogs, Templates, & File Upload Preferences info.',
 
-// General.
-'yes'		=> 'Yes',
-'no'		=> 'No',
-'input'		=> 'Input',
-'output'	=> 'Output',
-'minutes'	=> 'minutes',
+	'sl_developer_info_module_name'					=> "SL Developer Info",
+	'sl_developer_info_module_description'	=> "Collates Weblogs, Templates, & File Upload Preferences info.",
 
-// Navigation.
-'nav_templates'	=> 'Template Groups',
-'nav_weblogs'	=> 'Weblogs',
-'nav_files'		=> 'File Upload Locations',
-'nav_globals'	=> 'Global Variables',
-'nav_prefs'		=> '$PREFS Object',
-'nav_jump_to'	=> 'Jump to',
-'nav_go'		=> 'Go',
+	// General.
+	'yes'			=> "Yes",
+	'no'			=> "No",
+	'input'		=> "Input",
+	'output'	=> "Output",
+	'minutes'	=> "minutes",
 
-// Module home page.
-'home_title'	=> 'SL Developer Info',
-'home_intro'	=> 'Conveniently displays template, weblog, and file upload information in one place.',
+	// Navigation.
+	'nav_templates'			=> "Template Groups",
+	'nav_weblogs'				=> "Weblogs",
+	'nav_files'					=> "File Upload Locations",
+	'nav_globals'				=> "Global Variables",
+	'nav_prefs'         => '$PREFS Object',
+	'nav_jump_to'				=> "Jump to",
+	'nav_go'						=> "Go",
 
-// Templates.
-'templates_meta_title'	=> 'SL Developer Info : Template Groups',
-'templates_title'		=> 'Template Groups',
-'templates_action_link'	=> 'Template Management',
-'templates_no_groups'	=> 'There are no Template Groups.',
-'templates_no_templates' => 'There are no Templates in this Template Group.',
+	// Module home page.
+	'home_title'				=> "SL Developer Info",
+	'home_intro'				=> "Conveniently displays template, weblog, and file upload information in one place.",
 
-'template_edit_prefs'	=> 'Edit Preferences',
+	// Templates.
+	'templates_meta_title'		=> "SL Developer Info : Template Groups",
+	'templates_title'					=> "Template Groups",
+	'templates_action_link'		=> "Template Management",
+	'templates_no_groups'			=> "There are no Template Groups.",
+	'templates_no_templates'	=> "There are no Templates in this Template Group.",
 
-'template_type_css'		=> 'CSS Stylesheet',
-'template_type_js'		=> 'JavaScript',
-'template_type_rss'		=> 'RSS Page',
-'template_type_static'	=> 'Static',
-'template_type_webpage'	=> 'Web Page',
-'template_type_xml'		=> 'XML',
-'template_type_unknown'	=> 'Unknown Template Type',	
+	'template_edit_prefs'		=> "Edit Preferences",
 
-'template_name'			=> 'Template Name',
-'template_type'			=> 'Type',
-'template_file'			=> 'Saved As File?',
-'template_cache'		=> 'Caching Enabled?',
-'template_php'			=> 'PHP Enabled?',
+	'template_type_css'			=> "CSS Stylesheet",
+	'template_type_js'			=> "JavaScript",
+	'template_type_rss'			=> "RSS Page",
+	'template_type_static'	=> "Static",
+	'template_type_webpage'	=> "Web Page",
+	'template_type_xml'			=> "XML",
+	'template_type_unknown'	=> "Unknown Template Type",
 
-// Weblogs.
-'weblogs_meta_title'	=> 'SL Developer Info : Weblogs',
-'weblogs_title'			=> 'Weblogs',
-'weblogs_action_link'	=> 'Weblog Administration',
-'weblogs_no_weblogs'	=> 'There are no Weblogs.',
+	'template_name'		=> "Template Name",
+	'template_type'		=> "Type",
+	'template_file'		=> "Saved As File?",
+	'template_cache'	=> "Caching Enabled?",
+	'template_php'		=> "PHP Enabled?",
 
-'weblog_edit_prefs'		=> 'Edit Preferences',
-'weblog_edit_groups'	=> 'Edit Groups',
-'weblog_edit_field_group' => 'Edit Field Group',
+	// Weblogs.
+	'weblogs_meta_title'		=> "SL Developer Info : Weblogs",
+	'weblogs_title'					=> "Weblogs",
+	'weblogs_action_link'		=> "Weblog Administration",
+	'weblogs_no_weblogs'		=> "There are no Weblogs.",
 
-'weblog_categories'		=> 'Categories',
-'weblog_statuses'		=> 'Statuses',
+	'weblog_edit_prefs'			=> "Edit Preferences",
+	'weblog_edit_groups'		=> "Edit Groups",
+	'weblog_edit_field_group' => "Edit Field Group",
 
-'weblog_no_fields'		=> 'There are no fields in this Weblog.',
-'weblog_no_categories'	=> 'No categories.',
-'weblog_no_statuses'	=> 'No statuses.',
+	'weblog_categories'			=> "Categories",
+	'weblog_statuses'				=> "Statuses",
 
-'weblog_field_id'		=> 'ID',
-'weblog_field_name'		=> 'Short Name',
-'weblog_field_label'	=> 'Label',
-'weblog_field_type'		=> 'Type',
-'weblog_field_format'	=> 'Format',
-'weblog_field_mandatory' => 'Mandatory?',
-'weblog_field_gypsy'	=> 'Gypsy?',
+	'weblog_no_fields'			=> "There are no fields in this Weblog.",
+	'weblog_no_categories'	=> "No categories.",
+	'weblog_no_statuses'		=> "No statuses.",
 
-'weblog_field_type_text'	=> 'Text Input',
-'weblog_field_type_textarea' => 'Textarea',
-'weblog_field_type_select'	=> 'Drop-down List',
-'weblog_field_type_date'	=> 'Date Field',
-'weblog_field_type_rel'		=> 'Relationship',
-'weblog_field_type_unknown'	=> 'Unknown',
+	'weblog_field_id'					=> "ID",
+	'weblog_field_name'				=> "Short Name",
+	'weblog_field_label'			=> "Label",
+	'weblog_field_type'				=> "Type",
+	'weblog_field_format'			=> "Format",
+	'weblog_field_mandatory'	=> "Mandatory?",
+	'weblog_field_gypsy'      => "Gypsy?",
 
-// Files.
-'files_meta_title'		=> 'SL Developer Info : File Upload Locations',
-'files_title'			=> 'File Upload Locations',
-'files_action_link'		=> 'File Upload Preferences',
+	'weblog_field_type_text'			=> "Text Input",
+	'weblog_field_type_textarea'	=> "Textarea",
+	'weblog_field_type_select'		=> "Drop-down List",
+	'weblog_field_type_date'			=> "Date Field",
+	'weblog_field_type_rel'				=> "Relationship",
+	'weblog_field_type_unknown'		=> "Unknown",
 
-'file_location_id'		=> 'ID',
-'file_location_name'	=> 'Name',
-'file_location_path'	=> 'Server Path',
-'file_location_valid'	=> 'Valid?',
-'file_location_writeable' => 'Writeable?',
-'file_location_url'		=> 'URL',
-'file_location_types'	=> 'Allowed File Types',
-'file_location_size'	=> 'Max. Size (KB)',
+	// Files.
+	'files_meta_title'			=> "SL Developer Info : File Upload Locations",
+	'files_title'						=> "File Upload Locations",
+	'files_action_link'			=> "File Upload Preferences",
 
-'file_type_image'		=> 'Images',
-'file_type_all'			=> 'Any',
-'file_type_unknown'		=> 'Unknown',
+	'file_location_id'			=> "ID",
+	'file_location_name'		=> "Name",
+	'file_location_path'		=> "Server Path",
+	'file_location_url'			=> "URL",
+	'file_location_types'		=> "Allowed File Types",
+	'file_location_size'		=> "Max. Size (KB)",
 
-// Globals.
-'globals_meta_title'	=> 'SL Developer Info : Global Variables',
-'globals_title'			=> 'Global Variables',
-'globals_action_link'	=> 'Global Variables',
+	'file_type_image'				=> "Images",
+	'file_type_all'					=> "Any",
+	'file_type_unknown'			=> "Unknown",
 
-'globals_no_vars'		=> 'There are no Global Variables to display.',
+	// Globals.
+	'globals_meta_title'		=> "SL Developer Info : Global Variables",
+	'globals_title'					=> "Global Variables",
+	'globals_action_link'		=> "Global Variables",
 
-'globals_var_id'		=> 'ID',
-'globals_var_name'		=> 'Name',
-'globals_var_value'		=> 'Value',
+	'globals_no_vars'				=> "There are no Global Variables to display.",
 
-// PREFS
-'prefs_meta_title'	=> 'SL Developer Info : $PREFS',
-'prefs_title'		=> '$PREFS Object',
-'prefs_id'			=> 'ID',
-'prefs_value'		=> 'Value',
-'prefs_no_properties' => 'There are no properties to display.',
+	'globals_var_id'				=> 'ID',
+	'globals_var_name'			=> 'Name',
+	'globals_var_value'			=> 'Value',
 
-// All done.
-'' => ''
+	// PREFS
+	'prefs_meta_title'      => 'SL Developer Info : $PREFS',
+	'prefs_title'           => '$PREFS Object',
+	'prefs_id'              => "ID",
+	'prefs_value'           => "Value",
+	'prefs_no_properties'   => "There are no properties to display.",
+
+	//BOS Extra Stuff
+	'nav_modules'				=> "Modules",
+	'nav_extensions'		=> "Extensions",
+	'nav_plugins'				=> "Plugins",
+	'nav_fieldframe'		=> "Field Frame Settings",
+	'nav_customfields'	=> "Custom Field Groups",
+	'nav_wygwam'				=> "Wygwam Configs",
+	'weblog_field_searchable' => "Searchable?",
+	'weblog_field_group' 			=> "Field Group",
+	'weblog_field_directory' 	=> "Directory/Matrix Cols",
+	'weblog_no_field_group'	=> "No field group.",
+	'custom_field_box_head' => "All Custom Field Types in Use",
+	'view_entries'			=> "View Entries",
+	'no_upload_dir'			=> "No Upload Directory",
+
+	// All done.
+	'' => ''
 );
 
 ?>
